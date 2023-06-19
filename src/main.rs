047cb689f52f6640211da642d655bfb79e8d2b33
Daniel Pecos Martinez <me@danielpecos.com>
2023-06-19 21:40:08 +0200

    Update max_tokens and add stop parameter to text-davinci-003 model

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 "model": "text-davinci-003",
                 "prompt": prompt,
                 "temperature": 0.0,
-                "max_tokens": 150,
+                "max_tokens": 300,
                 "top_p": 1.0,
                 "frequency_penalty": 0.0,
                 "presence_penalty": 0.0,
                 "stream": false,
+                "stop": ["\n\n\n"],
             }))
             .send()
             .await?;
